29a819d7ffd36d01ba41946199110ff8982368aa
Ilya Kolbin <iskolbin@gmail.com>
2016-09-15 16:17:00 +0300

    updated for new luabox

--- test.lua
+++ test.lua
@@ -22,11 +22,11 @@ local function render()
 	Luabox.clear()
 	for x = 1, width do
 		for y = 1, height do 
-			Luabox.setcell( field[x][y] > 0 and '#' or ' ', x, y, 0, Luabox.rgb( red:get(x,y), green:get(x,y), blue:get(x,y)))
+			Luabox.setcell( field[x][y] > 0 and '#' or ' ', x, y, 0, Luabox.rgbf( red:get(x,y), green:get(x,y), blue:get(x,y)))
 			--Luabox.gray( light[x] and light[x][y] or 0 ))	
 		end
 	end
-	Luabox.setcell( '@', playerx, playery, Luabox.rgb(1,0,0))
+	Luabox.setcell( '@', playerx, playery, Luabox.rgbf(1,0,0))
 	Luabox.present()
 end
 
